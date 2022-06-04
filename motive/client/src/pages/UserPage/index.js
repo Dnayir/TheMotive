272cc2582c2610771c6e3a6c567a6635251018f0
“iAmash412” <aafthab.ashraff@gmail.com>
2022-06-04 23:20:36 +0100

    Logout function

--- motive/client/src/pages/UserPage/index.js
+++ motive/client/src/pages/UserPage/index.js
@@ -4,6 +4,13 @@ import httpClient from '../httpClient';
 const UserPage = () => {
   const [user, setUser] = useState({ id: '', email: '' });
 
+  const logoutUser = async () => {
+    const resp = await httpClient.post(
+      'https://the-motive-one.herokuapp.com/logout'
+    );
+    window.location.href = '/';
+  };
+
   useEffect(() => {
     (async () => {
       try {
@@ -25,15 +32,18 @@ const UserPage = () => {
           <h1>Logged In</h1>
           <h3>Email: {user.email}</h3>
           <h3>ID: {user.id}</h3>
+          <div>
+            <button onClick={logoutUser()}>Logout</button>
+          </div>
         </div>
       ) : (
         <div>
           <p>You are not logged in</p>
           <div>
-            <a href="/login">
+            <a href="/Login">
               <button>Login</button>
             </a>
-            <a href="/register">
+            <a href="/Register">
               <button>Register</button>
             </a>
           </div>
