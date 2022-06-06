b060d3bdd4054e33da4bfaa2c013173f14ecdb56
kharileigh <kharileighmiller@gmail.com>
2022-06-06 22:21:13 +0100

    NAVBAR STYLED FOR MOBILE DEVICE

--- motive/client/src/components/NavBar/index.js
+++ motive/client/src/components/NavBar/index.js
@@ -14,56 +14,97 @@ const NavBar = () => {
     // const username = useSelector(state => state.username)
 
     return (
-        <nav>
-            <div className='left-corner'>
-                <div className='image-container'>
-                    <img src={logo} 
-                        width='250' 
-                        height='220' 
-                        alt='the.MOTIVE Logo'
-                        style={{
-                            display: 'flex',
-                            justifyContent: 'center',
-                            alignItems: 'flex-start',
-                            alignSelf: 'center',
-                            marginRight: '225px',
-                            margin: '0',
-                            
-                        }} />
-                </div>
+
+            <nav>
+
+                <div className='nav-container'>
                     
-            </div>
-
-            {/* --- USE WHEN USERNAME FUNCTIONALITY CREATED */}
-            {/* <div className='right-corner'>
-                {username && navBarLinks}
-            </div> */}
-
-            <div className='right-corner'>
-
-                {/* ---- MODAL BOX NEEDED FOR LOCATION ----- */}
-                <NavLink role='link' className='nav-item' to='/location'>
-                    <div className='icon-container'>
-                        <img src={location} width='100' height='100' alt='Location' />
-
+                    <div className='image-container'>
+                        <img src={logo} 
+                            width='180' 
+                            height='160' 
+                            alt='the.MOTIVE Logo'
+                            style={{
+                                display: 'flex',
+                                justifyContent: 'center',
+                                alignItems: 'flex-start',
+                                alignSelf: 'center',
+                                
+                                marginLeft: '55px',
+                                marginTop: '5px',
+                                margin: '0',
+                                
+                            }} />
                     </div>
-                </NavLink>
+                        
 
-                <NavLink role='link' className='nav-item' to='/friends'>
-                    <div className='icon-container'>
-                        <img src={friends} width='100' height='100' alt='Location' />
+                    {/* ---- MODAL BOX NEEDED FOR LOCATION ----- */}
+                    <NavLink role='link' className='nav-item' to='/location'>
+                        <div className='icon-container'>
+                            <img src={location} 
+                                width='60' 
+                                height='80' 
+                                alt='Location'
+                                style={{
+                                    display: 'flex',
+                                    justifyContent: 'center',
+                                    alignItems: 'flex-start',
+                                    alignSelf: 'center',
+                                    marginRight: '225px',
+                                    marginLeft: '-22px',
+                                    marginTop: '-170px',
+                                    margin: '0',
+                                    
+                                }} />
 
-                    </div>
-                </NavLink>
+                        </div>
+                    </NavLink>
 
-                <NavLink role='link' className='nav-item' to='/friends'>
-                    <div className='icon-container'>
-                        <img src={logout} width='100' height='100' alt='Location' />
+                    <NavLink role='link' className='nav-item' to='/friends'>
+                        <div className='icon-container'>
+                            <img src={friends} 
+                                width='60' 
+                                height='80' 
+                                alt='Location'
+                                style={{
+                                    display: 'flex',
+                                    justifyContent: 'center',
+                                    alignItems: 'center',
+                                    alignSelf: 'center',
+                                    marginRight: '225px',
+                                    marginLeft: '-26px',
+                                    marginTop: '-170px',
+                                    margin: '0',
+                                    
+                                }} />
 
-                    </div>
-                </NavLink>
-            </div>
-        </nav>
+                        </div>
+                    </NavLink>
+
+                    <NavLink role='link' className='nav-item' to='/friends'>
+                        <div className='icon-container'>
+                            <img src={logout} 
+                                width='60' 
+                                height='80' 
+                                alt='Location'
+                                style={{
+                                    display: 'flex',
+                                    justifyContent: 'center',
+                                    alignItems: 'flex-start',
+                                    alignSelf: 'center',
+                                    marginRight: '225px',
+                                    marginLeft: '-22px',
+                                    marginTop: '-90px',
+                                    margin: '0',
+                                    
+                                }} />
+
+                        </div>
+                    </NavLink>
+
+                </div>
+            </nav>
+            
     );
 }
 
