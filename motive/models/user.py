d40d59e6192a346c04dfd91957771f062999cc4d
WalkingZ3d <danielrwillis@hotmail.co.uk>
2022-06-06 10:37:00 +0100

    added reviews in db

--- motive/models/user.py
+++ motive/models/user.py
@@ -3,19 +3,39 @@ from uuid import uuid4
 
 db = SQLAlchemy()
 
+
 def get_uuid():
     return uuid4().hex
 
 
+class Reviews(db.Model):
+    __tablename__ = 'reviews'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
+    user = db.relationship('Users', backref='user')
+    review_id = db.Column(db.Integer, db.ForeignKey(
+        'users.id'))
+
+
 class User(db.Model):
     __tablename__ = 'users'
-    id = db.Column(db.String(32), primary_key=True, unique=True, default=get_uuid)
+    id = db.Column(db.Integer, primary_key=True,
+                   unique=True, default=get_uuid)
     email = db.Column(db.String(500), unique=True)
     password = db.Column(db.String(255), nullable=False)
-#     reviews = db.relationship("Reviews")
+    reviews = db.relationship('Reviews', backref='reviewer')
+#     friends = db.relationship('Friends', backref='friends')
 
 
-# class Reviews(db.Model):
-
+# class Friends(db.Model):
+#     __tablename__ = 'friends'
 #     id = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.String(80))
+#     friend_request = db.Column(db.Integer,
+#                                db.ForeignKey(
+#                                    'users.id', ondelete='CASCADE', onupdate='CASCADE'),
+#                                nullable=False)
+#     friend_accept = db.Column(db.Integer,
+#                               db.ForeignKey(
+#                                   'users.id', ondelete='CASCADE', onupdate='CASCADE'),
+#                               nullable=False)
+#     user = db.relationship('Users', backref='user')
