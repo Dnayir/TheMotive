2d6da626141006975fb203d4012e704f36c7b268
“iAmash412” <aafthab.ashraff@gmail.com>
2022-06-09 12:45:40 +0100

    COOKIES

--- motive/client/src/pages/DrinkPage/index.js
+++ motive/client/src/pages/DrinkPage/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useNavigate, NavLink } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 
@@ -11,117 +11,110 @@ import { loadDrinkCategory } from '../../actions';
 import TypeWriterEffect from 'react-typewriter-effect';
 
 const DrinkPage = () => {
+  useEffect(() => {
+    console.log(document.cookie);
+  });
 
-    // ----> NEEDS NEW DRINK ANIMATED ICONS!
+  // ----> NEEDS NEW DRINK ANIMATED ICONS!
 
-    const navigate = useNavigate();
-    const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
-    function handleBar(e) {
-        // NEED TO SPECIFY IT IS A BAR CATEGORY
-        let updateBar = selectedBar => dispatch(loadDrinkCategory(selectedBar));
-        updateBar(e.currentTarget.value)
-        navigate('/venues');
-    }
+  function handleBar(e) {
+    // NEED TO SPECIFY IT IS A BAR CATEGORY
+    let updateBar = (selectedBar) => dispatch(loadDrinkCategory(selectedBar));
+    updateBar(e.currentTarget.value);
+    navigate('/venues');
+  }
 
-    function handlePub(e) {
-        // NEED TO SPECIFY IT IS A PUB CATEGORY
-        let updateBar = selectedPub => dispatch(loadDrinkCategory(selectedPub));
-        updateBar(e.currentTarget.value)
-        navigate('/venues');
-    }
-   
-    return (
-        <div className='main-container'>
-       
-                <NavBar />
-                <div className='typewriter'>
-                    <TypeWriterEffect 
-                        textStyle={{
-                            color: '#4DA537',
-                            textShadow: '2px 2px 2px  #FF5900',
-                            fontFamily: 'monospace',
-                            fontWeight: 'bold',
-                            fontSize: '3.7em',
-                            textAlign: 'center',
-                            lineHeight: '1.3',
+  function handlePub(e) {
+    // NEED TO SPECIFY IT IS A PUB CATEGORY
+    let updateBar = (selectedPub) => dispatch(loadDrinkCategory(selectedPub));
+    updateBar(e.currentTarget.value);
+    navigate('/venues');
+  }
 
-                            display: 'flex',
-                            alignItems: 'center',
-                            justifyContent: 'center',
-                            marginTop: '100px',
-                        }}
-                        cursorColor='#da3422'
-                        text='WHAT\S THE VIBE?'
-                        typeSpeed={70}
-                        startDelay={0.1}
-                        loop={true}
-                    
-                    />
-                </div>
+  return (
+    <div className="main-container">
+      <NavBar />
+      <div className="typewriter">
+        <TypeWriterEffect
+          textStyle={{
+            color: '#4DA537',
+            textShadow: '2px 2px 2px  #FF5900',
+            fontFamily: 'monospace',
+            fontWeight: 'bold',
+            fontSize: '3.7em',
+            textAlign: 'center',
+            lineHeight: '1.3',
 
-                <br></br>
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            marginTop: '100px',
+          }}
+          cursorColor="#da3422"
+          text="WHAT\S THE VIBE?"
+          typeSpeed={70}
+          startDelay={0.1}
+          loop={true}
+        />
+      </div>
 
-                <div className='category-container'>
-                    
-                    <div className='top-container'>
-                        <NavLink role='link' className='nav-item' to='/venues'>
-                            <div className='image-container'>
-                                <img 
-                                // BAR ANIMATED ICON NEEDED
+      <br></br>
 
-                                    src={barGif}
-                                    onClick={handleBar}
-                                    value='bar'
-                                    width='170'
-                                    height='200'
-                                    alt='Food Gif'
-                                    style={{
-                                        display: 'flex',
-                                        justifyContent: 'center',
-                                        alignItems: 'flex-end',
-                                        alignSelf: 'center',
-                                        marginRight: 'auto',
-                                        marginTop: '-40px',
-                                        
-                                    }}
-                                />
-                            </div>
-                        </NavLink>
-                    </div>
-                    <div className='bottom-container'>
-                        <NavLink role='link' className='nav-item' to='/venues'>
-                            <div className='image-container'>
-                                <img 
-                                // PUB ANIMATED ICON NEEDED
+      <div className="category-container">
+        <div className="top-container">
+          <NavLink role="link" className="nav-item" to="/venues">
+            <div className="image-container">
+              <img
+                // BAR ANIMATED ICON NEEDED
 
-                                        src={pubGif} 
-                                        onClick={handlePub}
-                                        value='Pub'
-                                        width='170'
-                                        height='200'
-                                        alt='Food Gif'
-                                        style={{
-                                            display: 'flex',
-                                            justifyContent: 'center',
-                                            alignItems: 'flex-end',
-                                            alignSelf: 'center',
-                                            marginRight: 'auto',
-                                            marginTop: '-10px',
-                                            
-                                        }}
-                                 />
-                            </div>
-                        </NavLink>
-                    </div>
-
-                </div>
-            
-            
+                src={barGif}
+                onClick={handleBar}
+                value="bar"
+                width="170"
+                height="200"
+                alt="Food Gif"
+                style={{
+                  display: 'flex',
+                  justifyContent: 'center',
+                  alignItems: 'flex-end',
+                  alignSelf: 'center',
+                  marginRight: 'auto',
+                  marginTop: '-40px',
+                }}
+              />
+            </div>
+          </NavLink>
         </div>
-    )
+        <div className="bottom-container">
+          <NavLink role="link" className="nav-item" to="/venues">
+            <div className="image-container">
+              <img
+                // PUB ANIMATED ICON NEEDED
 
-}
-
+                src={pubGif}
+                onClick={handlePub}
+                value="Pub"
+                width="170"
+                height="200"
+                alt="Food Gif"
+                style={{
+                  display: 'flex',
+                  justifyContent: 'center',
+                  alignItems: 'flex-end',
+                  alignSelf: 'center',
+                  marginRight: 'auto',
+                  marginTop: '-10px',
+                }}
+              />
+            </div>
+          </NavLink>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default DrinkPage;
