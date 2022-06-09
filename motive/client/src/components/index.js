42516908d72758a3d2031978123d226664b403e8
kharileigh <kharileighmiller@gmail.com>
2022-06-09 01:12:46 +0100

    DELETED ANIMATED FILES RELATED TO CARD
    not working properly

--- motive/client/src/components/index.js
+++ motive/client/src/components/index.js
@@ -1,5 +1,5 @@
 export { Deploy } from './Deploy';
-export { default as Card } from './Card';
+export { default as Venue } from './Venue';
 export { CurrentLocation } from './CurrentLocation';
 // export { default as LocationModal } from './LocationModal';
 export { default as NavBar } from './NavBar';
