f1fd68b9e19feb9f311af566ff12b97942f037e5
“iAmash412” <aafthab.ashraff@gmail.com>
2022-06-09 10:57:13 +0100

    removed /data and deploy comp

--- motive/client/src/components/index.js
+++ motive/client/src/components/index.js
@@ -1,4 +1,4 @@
-export { Deploy } from './Deploy';
+
 export { default as Venue } from './Venue';
 export { CurrentLocation } from './CurrentLocation';
 // export { default as LocationModal } from './LocationModal';
